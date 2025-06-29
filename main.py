a56724a298dc50d903349defc368412732e9eb10
adriaurora <adrianlab@proton.me>
2025-06-29 21:43:18 +0200

    add clock to controlf FPS

---
+++ main.py
@@ -0,0 +1,26 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()  # initialize the pygame library
+    clock = pygame.time.Clock()  # create a clock to control the frame rate
+    dt = 0  # delta time, used to control the speed of the game
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}"
+          f"\nScreen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000.0  # limit to 60 FPS
+
+
+if __name__ == "__main__":
+    main()
