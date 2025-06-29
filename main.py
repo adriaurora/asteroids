8380553569911fc79d8e00a93a09ca1c50ac8c9c
adriaurora <adrianlab@proton.me>
2025-06-29 22:01:51 +0200

    add player and circleshape

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
 
 
 def main():
@@ -10,6 +12,7 @@ def main():
     clock = pygame.time.Clock()  # create a clock to control the frame rate
     dt = 0  # delta time, used to control the speed of the game
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}"
           f"\nScreen height: {SCREEN_HEIGHT}")
@@ -18,6 +21,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0  # limit to 60 FPS
 
